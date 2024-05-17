c6f5740e0737764b12f5cd1dbd4bc4b1b3da395c
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-05-17 18:04:42 -0300

    Feat.: PresenteComponent.vue

---
+++ form_game/src/components/PresenteComponent.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container-fluid">
+    <div class="d-flex flex-column mx-5">
+      <div class="row d-flex align-items-center justify-content-center mt-3">
+        <div class="col-6">
+          <a href="https://juntos.art.br" target="blank" style="text-decoration: none">
+            <div class="row d-flex justify-content-start align-items-center">
+              <div class="col-12 my-auto text-center" style="width: auto">
+                <img
+                  src="../assets/Logo Juntos.png"
+                  alt="Logo da JunTOs"
+                  class="img-fluid img-logo"
+                />
+              </div>
+              <!-- <div class="col-6 my-auto" style="width: auto">
+                  <div class="row text-center">
+                    <div class="col logo-text-title">JUNTOS</div>
+                  </div>
+                  <div class="row text-center">
+                    <div class="col logo-text-sub">METAVERSOS</div>
+                  </div>
+                  <div class="row text-center">
+                    <div class="col logo-text-sub">TERAPÊUTICOS</div>
+                  </div>
+                </div> -->
+            </div>
+          </a>
+        </div>
+        <div class="col-6">
+          <div class="d-flex justify-content-end align-items-center">
+            <div class="close">X</div>
+          </div>
+        </div>
+      </div>
+      <div class="row scroller"></div>
+      <h1 class="mt-5">Lorem Ipsum</h1>
+    </div>
+    <div class="row text-box my-3">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi feugiat dolor eu consequat
+        placerat. Nunc viverra purus vulputate ante gravida, finibus suscipit tellus pulvinar. Cras
+        a risus ac sapien porttitor aliquam tempor nec felis. Donec non magna non lectus molestie
+        iaculis. Suspendisse pharetra dui vitae leo suscipit congue.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  font-family: 'Roboto', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  text-align: center;
+  font-weight: bold;
+  font-size: 24pt;
+}
+.close {
+  color: #addad3;
+  font-size: 24pt;
+}
+.close:hover {
+  cursor: pointer;
+}
+.text-block {
+  position: absolute;
+  width: 100%;
+  margin-top: 500px;
+  z-index: 10;
+  height: 100%;
+  max-height: 1340px;
+  overflow-y: scroll;
+  margin-bottom: 2rem;
+}
+p {
+  padding: 0 1.5rem;
+  text-align: justify;
+}
+</style>
--- form_game/src/router/index.ts
+++ form_game/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import LandingView from '@/views/LandingView.vue'
 import FormsView from '@/views/FormsView.vue'
 import LandingViewNew from '@/views/LandingViewNew.vue'
+import PresenteComponent from '@/components/PresenteComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/form',
       name: 'formsView',
       component: FormsView
+    },
+    {
+      path: '/presente',
+      name: 'Presente',
+      component: PresenteComponent
     }
 
     // {
